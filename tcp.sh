3a71d831fe6da088666b9d06b8eed70cb3b3e859
school-life-VII <90141646+school-life-of-agv@users.noreply.github.com>
2022-10-28 14:19:50 +0530

    Update tcp.sh

--- tcp.sh
+++ tcp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 main(){
 
-./ngrok/ngrok.exe config add-authtoken 2GcOiSIIzipf2UEFhN6231fbViF_2niJXUrPLDjnhHSv3vXD8
+./ngrok/ngrok.exe authtoken 2GcOiSIIzipf2UEFhN6231fbViF_2niJXUrPLDjnhHSv3vXD8
 ./ngrok/ngrok.exe tcp 3389
 sleep 5
 echo sucess
-./ngrok/ngrok.exe config add-authtoken 1kizhCykLZXaokTtJ82xbCCNbCz_5dvTZxFef6mjKEVLfiP8o
+./ngrok/ngrok.exe authtoken 1kizhCykLZXaokTtJ82xbCCNbCz_5dvTZxFef6mjKEVLfiP8o
 ./ngrok/ngrok.exe tpc 3389
 sleep 5
 echo sucess
