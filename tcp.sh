f92acc35956ecb61422c15d8bb18ddd781062cc7
school-life-VII <90141646+school-life-of-agv@users.noreply.github.com>
2022-10-29 11:08:46 +0530

    Update tcp.sh

--- tcp.sh
+++ tcp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while :
+while true:
 do
 ./ngrok/ngrok.exe authtoken 2GcOiSIIzipf2UEFhN6231fbViF_2niJXUrPLDjnhHSv3vXD8
 ./ngrok/ngrok.exe tcp 3389
@@ -11,6 +11,5 @@ echo sucess
 sleep 5
 echo sucess
 sleep 2
-num = 0
 done
 
